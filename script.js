2c4212b67be0f851cbfba25bb8b65094d1839286
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-02-02 10:59:02 -0600

    Bonus completed

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let warmHugs = "Hi, I'm Olaf and I like warm hugs."
 console.log(warmHugs);
 console.log(warmHugs.toUpperCase)
-console.log(warmHugs.replace ("like", "love"))
+warmHugs = warmHugs.replace ("like", "love")
+console.log(warmHugs)
 var beenImpaled = "Oh, look at that. I've been impaled."
 console.log(beenImpaled.slice (18));
 var dotDotDot = ". . ."
@@ -12,3 +13,14 @@ var randomNumber = Math.random() *3 +1
 randomNumber = Math.floor (randomNumber)
 console.log(randomNumber);
 
+//BONUS
+console.log(" Let It Go!".toUpperCase().repeat(2).trim());
+let reindeers = "Reindeers are better than people"
+console.log(reindeers.replaceAll(" ", "_"));
+console.log(Math.SQRT2);
+// var newRandomNumber = Math.floor(Math.random() * 17) +7
+let newRandomNumber = Math.random()
+newRandomNumber *= 17
+newRandomNumber += 7
+newRandomNumber = Math.floor(newRandomNumber)
+console.log(newRandomNumber);
